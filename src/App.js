3fc476396961b663bfaa88da20087e100316c57d
Justin Haldeman <jmanh14@gmail.com>
2020-08-18 09:55:19 -0500

    Added lyric page with info and added search field

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Navbar from './componets/layout/Navbar';
-import Index from './componets/layout/Index';
-import {Provider} from './context';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./componets/layout/Navbar";
+import Index from "./componets/layout/Index";
+import Lyrics from "./componets/tracks/Lyrics";
+import { Provider } from "./context";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Provider>
-    <Router>
-    <React.Fragment>
-      <Navbar/>
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Index}/>
-        </Switch>
-      </div>
-    </React.Fragment>
-    </Router>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     </Provider>
   );
 }
---
+++ src/componets/tracks/Search.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Consumer } from "../../context";
+
+class Search extends Component {
+  state = {
+    trackTitle: "",
+  };
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <Consumer>
+        {(value) => {
+          return (
+            <div className="card card-body mb-4 p-4">
+              <h1 className="display-4 text-center">
+                <i className="fas fa-music"></i> Search For A Song
+              </h1>
+              <p className="lead text-center">Get Lyrics For Any Song</p>
+              <form>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Song Title..."
+                    name="trackTitle"
+                    value={this.state.trackTitle}
+                    onChange={this.onChange}
+                  />
+                </div>
+              </form>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Search;
