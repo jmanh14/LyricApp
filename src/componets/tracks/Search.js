384f3cd203edb072d3bc732dc0b270041ce2768f
Justin Haldeman <jmanh14@gmail.com>
2020-08-18 10:14:39 -0500

    Search working, filters tracks to search

--- src/componets/tracks/Search.js
+++ src/componets/tracks/Search.js
@@ -7,6 +7,22 @@ class Search extends Component {
     trackTitle: "",
   };
 
+  findTrack = (dispatch, e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((res) => {
+        dispatch({
+          type: "SEARCH_TRACKS",
+          payload: res.data.message.body.track_list,
+        });
+        this.setState({ trackTitle: "" });
+      })
+      .catch((err) => console.log(err));
+  };
+
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
@@ -15,13 +31,14 @@ class Search extends Component {
     return (
       <Consumer>
         {(value) => {
+          const { dispatch } = value;
           return (
             <div className="card card-body mb-4 p-4">
               <h1 className="display-4 text-center">
                 <i className="fas fa-music"></i> Search For A Song
               </h1>
               <p className="lead text-center">Get Lyrics For Any Song</p>
-              <form>
+              <form onSubmit={this.findTrack.bind(this, dispatch)}>
                 <div className="form-group">
                   <input
                     type="text"
